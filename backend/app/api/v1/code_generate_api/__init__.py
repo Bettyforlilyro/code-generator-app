491d77320f2a93e736324378848246ef1874f973
Bettyforlilyro <yz72047@gmail.com>
2026-09-05 02:01:58 +0800

    文件名拼写错误修正

--- backend/app/api/v1/code_generate_api/__init__.py
+++ backend/app/api/v1/code_generate_api/__init__.py
@@ -9,4 +9,4 @@ def register_code_generate_routes():
     注册代码生成器相关路由
     """
     # 延迟导入路由，确保在蓝图注册前完成所有路由定义
-    from backend.app.api.v1.code_generate_api import code_generate, code_resoure
+    from backend.app.api.v1.code_generate_api import code_generate, code_resource
